1ed1e25b70a20b338a63771e8b3c332fd2c761d6
DasLixou <einlixou@gmail.com>
2022-03-04 21:26:43 +0100

    🔃 Added Input and Output stream

--- src/main/java/cc/lixou/packy/PackyServer.java
+++ src/main/java/cc/lixou/packy/PackyServer.java
@@ -1,6 +1,9 @@
 package cc.lixou.packy;
 
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,13 +11,17 @@ public class PackyServer {
 
     private final ServerSocket serverSocket;
 
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
+
     public PackyServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
     }
 
     public void start() throws IOException {
         Socket socket = serverSocket.accept();
-        System.out.println("New client connected");
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        inputStream = new ObjectInputStream(socket.getInputStream());
     }
 
 }
