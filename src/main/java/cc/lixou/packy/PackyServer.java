2deb4f569a58f40b3614e59603462369e3d34e72
DasLixou <einlixou@gmail.com>
2022-03-08 16:41:02 +0100

    🤨 Changed from ServerSocket to Selector + Channels

--- src/main/java/cc/lixou/packy/PackyServer.java
+++ src/main/java/cc/lixou/packy/PackyServer.java
@@ -1,23 +1,23 @@
 package cc.lixou.packy;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 
 public class PackyServer {
 
-    private final ServerSocket serverSocket;
+    private final Selector selector;
+    private final ServerSocketChannel serverSocketChannel;
 
     private ObjectOutputStream outputStream;
     private ObjectInputStream inputStream;
 
     public PackyServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+        selector = Selector.open();
+        serverSocketChannel = ServerSocketChannel.open();
     }
 
     public void start() throws IOException {
-        Socket socket = serverSocket.accept();
         //outputStream = new ObjectOutputStream(socket.getOutputStream());
         //inputStream = new ObjectInputStream(socket.getInputStream());
     }
@@ -25,9 +25,10 @@ public class PackyServer {
     public void sendPacket(Packet packet) {
         PackyBuffer buffer = new PackyBuffer(null);
         packet.write(buffer);
-        PackyBuffer buffer1 = new PackyBuffer(buffer.result());
+        byte[] result = buffer.result();
         buffer.close();
-        System.out.println(buffer1.readBoolean());
+        packet.read(new PackyBuffer(result));
+        // TODO: Send Bytes
    }
 
 }
